f1f08b1865d49bc5f169c6f226548326892b93b1
Adsmendees11 <andersonmendees7@gmail.com>
2026-02-04 17:24:33 -0400

    Conexão MySQL usando variáveis de ambiente

--- src/db.js
+++ src/db.js
@@ -1,28 +1,12 @@
-/* OPÇÃO PARA DEIXAR APP ONLINE
-
 const mysql = require("mysql2/promise");
 
 // Configuração da conexão com o MySQL usando variáveis de ambiente
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,       // endereço do servidor MySQL (Railway/PlanetScale)
+  host: process.env.DB_HOST,       // endereço do servidor MySQL (Railway)
   user: process.env.DB_USER,       // usuário do banco
-  password: process.env.DB_PASS,   // senha do banco
+  password: process.env.DB_PASSWORD, // senha do banco (ajuste aqui: antes estava DB_PASS)
   database: process.env.DB_NAME,   // nome do banco
   port: process.env.DB_PORT || 3306 // porta padrão do MySQL
 });
 
-module.exports = pool; */
-
-const mysql = require("mysql2/promise");
-
-// Conexão direta com o banco de dados local
-const pool = mysql.createPool({
-  host: "localhost",      // endereço do servidor MySQL
-  user: "root",           // usuário do MySQL
-  password: "GarethBalee@711",  // senha do MySQL
-  database: "site_agendamento", // nome do banco
-  port: 3306              // porta padrão do MySQL
-});
-
 module.exports = pool;
-
